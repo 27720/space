c719bfc7c436b6f72ddb61c0e91cd84f3e305b45
76716 <farhannayyar77@gmail.com>
2026-05-29 07:17:42 +0100

    Modified for Keplerian orbit

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,42 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include "math.hpp"
 
 using namespace std;
 
 // Define the necessary shaders to allow drawing --
-
 const char* vertexShaderSrc = "#version 330 core\n"
-	"layout(location = 0) in vec3 aPos;\n"
+	"layout(location = 0) in vec2 aPos;\n"
 	"void main() {\n"
-    "	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "	gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);\n"
 	"}\0"; 
 
 const char* fragShaderSrc = "#version 330 core\n"
+	"uniform vec3 colour;\n"
 	"out vec4 fragColour;\n"
 	"void main() {\n"
-	"	fragColour = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+	"	fragColour = vec4(colour, 1.0f);\n"
 	"}\0";
+// -- 
 
+// Constants --
+constexpr float UNIT_RATIO = 0.322f; // Ratio of screen units to AU, numerous trials suggested 0.322 is optimal  
+
+// Simulation parameters relative to the units defined in math.hpp
+constexpr double STAR_MASS = 1.0;
+constexpr double A = 1.0; // Semi-major axis
+constexpr double E = 0.4; // Eccentricity
+constexpr double TIME_SCALE = 50.0; // i just messed w this until the earth stopped tping
 // --
 
-// Callback in case the window's size changes
+// Callbacks --
+// Window size changes
 void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
 
-// Self-explanatory
-// Will eventually include movement keys
+// Self-explanatory, will eventually include movement 
 void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
@@ -55,12 +65,13 @@ int main() {
 
 	glViewport(0, 0, 800, 600);
 
+	// I should probably make this process a function
 	// Declare Vertex Shader to help GL determine vertex positions
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSrc, NULL);
 	glCompileShader(vertexShader);
 
-	// Checks if compilation was successful --
+	// Check if compilation was successful --
 	int success;
 	char errorLog[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
@@ -76,7 +87,7 @@ int main() {
 	glShaderSource(fragShader, 1, &fragShaderSrc, NULL);
 	glCompileShader(fragShader);
 
-	// Checks if compilation was successful --
+	// Check if compilation was successful --
 	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &success);
 
 	if (!success) {
@@ -92,7 +103,7 @@ int main() {
 	glAttachShader(shader, fragShader);
 	glLinkProgram(shader);
 	
-	// Checks if link was successful --
+	// Check if link was successful --
 	glGetProgramiv(shader, GL_LINK_STATUS, &success);
 
 	if (!success) {
@@ -100,74 +111,83 @@ int main() {
 	}
 	// --
 
-	// Get rid of old shaders after linking them (not needed anymore)
+	// Get rid of the old shaders after linking them (not needed anymore)
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragShader);
 
-	// Setup vertex data
-	float triangle1[] = {
-		-0.875f, -0.5f, 0.0f,
-		-0.125f, -0.5f, 0.0f,
-		-0.5f, 0.5f, 0.0f
-	};
-
-	float triangle2[] = {
-		0.125f, -0.5f, 0.0f,
-		0.875f, -0.5f, 0.0f,
-		0.5f, 0.5f, 0.0f
-	};
+	// Define vertex data and the location of our uniform variable
+	int colourLocation = glGetUniformLocation(shader, "colour");
+	float vertices[] = {0.0f, 0.0f}; 
 
 	// Declare a buffer object to store vertices in the GPU
 	// Declare an array object to store VBO configurations
-	unsigned int VBO1, VBO2, VAO1, VAO2;
-	glGenVertexArrays(1, &VAO1);
-	glGenVertexArrays(1, &VAO2);
-	glGenBuffers(1, &VBO1);
-	glGenBuffers(1, &VBO2);
-	glBindVertexArray(VAO1);
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
 
 	// Select it, put shit into it, tell GL how to interpret it, and enable it
-	glBindBuffer(GL_ARRAY_BUFFER, VBO1);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle1), triangle1, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW); // GL_DYNAMIC_DRAW tells GL to buffer it somewhere fast since we gonna be messing with it
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindVertexArray(VAO2);
-
-	// Select it, put shit into it, tell GL how to interpret it, and enable it 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle2), triangle2, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	// Deselect everything
-	// Not necessary whatsoever asides from maybe deselecting the VBO since the VAO handles its selection in the render loop
+	// Deselect everything - a remnant of learngl.com cause i aint doing that
 	// glBindBuffer(GL_ARRAY_BUFFER, 0);
 	// glBindVertexArray(0);
+
+	// Set up orbital parameters
+	Orbit earth;
+	earth.a = A;
+	earth.e = E;
+
+	double inGameTime = 0.0; // ingame the funniest sh ive ever invented and it aint even close
+	double past = glfwGetTime(); 
 
 	// Render loop
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 		
-		glClear(GL_COLOR_BUFFER_BIT);
+		double present = glfwGetTime();
+		double dt = present - past;
+		inGameTime += dt * TIME_SCALE;
+		past = present; // w naming
 
-		glUseProgram(shader);
-		glBindVertexArray(VAO1);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glBindVertexArray(VAO2);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glClear(GL_COLOR_BUFFER_BIT);
 		
+		glUseProgram(shader);
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		
+		// Draw central star
+		float starVertices[] = {0.0f, 0.0f};
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(starVertices), starVertices);
+		glUniform3f(colourLocation, 1.0f, 0.0f, 0.0f); // p sure this is red
+		glPointSize(25.0f);
+		glDrawArrays(GL_POINTS, 0, 1);
+
+		// Determine where the body (in this case, Earth) is
+		glm::dvec2 earthPos = orbitPosition(earth, STAR_MASS, inGameTime);
+		float earthVertices[] = {
+			(float)(earthPos.x * UNIT_RATIO),
+			(float)(earthPos.y * UNIT_RATIO)
+        };
+
+		// Draw body - might add a trail later
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(earthVertices), earthVertices);
+		glUniform3f(colourLocation, 0.0f, 1.0f, 0.0f); // p sure this is blue
+		glPointSize(10.0f); // Not to scale, accurate shape though
+		glDrawArrays(GL_POINTS, 0, 1);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-	// janitor (slow rn fixing later)
-    glDeleteVertexArrays(1, &VAO1);
-    glDeleteBuffers(1, &VBO1);
-	glDeleteVertexArrays(1, &VAO2);
-    glDeleteBuffers(1, &VBO2);
+	// janitor
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
     glDeleteProgram(shader);
-
 	glfwTerminate();
+	
 	return 0;
 }
